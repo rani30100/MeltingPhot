1687d3c590ba0c8560e08ee60a76f8f27488c573
Rani Derradj <raniderradj2@gmail.com>
2023-04-17 15:57:47 +0200

    page actions prepa

---
+++ src/Controller/ActionsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ActionsController extends AbstractController
+{
+    #[Route('/actions', name: 'app_actions')]
+    public function index(): Response
+    {
+        return $this->render('actions/index.html.twig', [
+            'controller_name' => 'ActionsController',
+        ]);
+    }
+}
