12a8050bc25a38285144958abf4386f9e405026c
Rani Derradj <raniderradj2@gmail.com>
2023-04-18 10:57:17 +0200

    dsn mailer fait et controllerContact  fait

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,15 +2,36 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
+        if ($request->isMethod('POST')) {
+            $name = $request->request->get('name');
+            $email = $request->request->get('email');
+            $message = $request->request->get('message');
+
+            // Créer l'email
+            $email = (new Email())
+                ->from('noreply@example.com')
+                ->to('yourname@example.com')
+                ->subject('Nouveau message de contact')
+                ->html("Nom : $name<br>Email : $email<br>Message : $message");
+
+            // Envoyer l'email
+            $mailer->send($email);
+
+            return $this->redirectToRoute('contact_confirmation');
+        }
+
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
         ]);
