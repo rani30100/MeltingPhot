ab2cd59ca5f7c31315c6991b7131e057ea4104f4
Rani Derradj <raniderradj2@gmail.com>
2023-04-20 02:00:49 +0200

    Login /register/ CrudIMG/CRUDVideo/ telecharment du bundle pour afficher les errors 403

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\Translation\TranslatableMessage;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomePageController extends AbstractController
 {
     #[Route('/fr', name: 'app_home_page')]
-    public function index(TranslatorInterface $translator): Response
+    public function index(TranslatorInterface $translator,Security $security): Response
     {    
+        $user = $security->getUser();
 
         return $this->render('home_page/index.html.twig', [
       
--- src/Controller/admin/AdminController.php
+++ src/Controller/admin/AdminController.php
@@ -11,8 +11,18 @@ class AdminController extends AbstractController
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
+        
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
+    
+    public function adminDashboard(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        // or add an optional message - seen by developers
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+    }
+    
 }
