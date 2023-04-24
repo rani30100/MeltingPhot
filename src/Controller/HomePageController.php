54d0a3ab7f7ff6280afb2c2dc4097544b5c8b6bf
Rani Derradj <raniderradj2@gmail.com>
2023-04-24 11:42:36 +0200

    amélioration des routes pour /fr de base

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -12,12 +12,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function index(TranslatorInterface $translator,Security $security): Response
-    {    
+    public function index(TranslatorInterface $translator, Security $security): Response
+    {
         $user = $security->getUser();
 
-        return $this->render('home_page/index.html.twig', [
-      
-        ]);
+        return $this->render('home_page/index.html.twig', []);
     }
 }
