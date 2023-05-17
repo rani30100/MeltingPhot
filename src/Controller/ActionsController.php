7c20d4bbcb40d6350975603b5444324151bbb60b
Rani Derradj <raniderradj2@gmail.com>
2023-05-17 11:59:57 +0200

    Page actions, carroussel en cours / flipbook entame mais non rentre

--- src/Controller/ActionsController.php
+++ src/Controller/ActionsController.php
@@ -20,8 +20,6 @@ class ActionsController extends AbstractController
     #[Route('/actions/{order}/{year}', defaults: ['order' => 'test', 'year' => 'null'], methods: ['GET', 'HEAD'], name: 'app_actions')]
     public function index(string $order, string $year, EntityManagerInterface $entityManager, Request $request,Category $category = null): Response
     {
-        $cache = new FilesystemAdapter();
-        $videoEntities = $cache->get("videos_" . $order . "_" . $year, function (ItemInterface $item) use ($order, $entityManager) {
             // Créez un client API Google
             $client = new Client();
             $client->setApplicationName('MeltingPhot');
@@ -34,24 +32,26 @@ class ActionsController extends AbstractController
             $playlistMonfuturmétier = $youtube->playlistItems->listPlaylistItems('snippet', array(
                 'playlistId' => 'PLU8CGazlBJAsyM9QsrvGUK3SIDpUHngJl',
                 'maxResults' => 50,
-                'part' => 'snippet,status'
+                'part' => 'snippet,status',
             ));
 
-               // Récupérez la liste des vidéos de la playlist Odonymes
-               $playlistOdonymes = $youtube->playlistItems->listPlaylistItems('snippet', array(
-                'playlistId' => 'PLU8CGazlBJAsq1vzyPDWX5tEWT7aDS325',
-                'maxResults' => 50,
-                'part' => 'snippet'
-            ));
+            $filteredPlaylistItems = [];
+
+            foreach ($playlistMonfuturmétier->getItems() as $item) {
+                $status = $item->getStatus()->getPrivacyStatus();
+                if ($status === 'public' || $status === 'private') {
+                    $filteredPlaylistItems[] = $item;
+                }
+            }
             $videos1 = $playlistMonfuturmétier->getItems();
-            $videos2 = $playlistOdonymes ->getItems();
+         
 
             $videoEntities = [];
-            try {
+           
                 foreach ($videos1 as $videoData) {
                     $video = new Video();
                     $video->setName($videoData['snippet']['title']);
-                    $video->setUrl('https://www.youtube.com/watch?v=' . $videoData['snippet']['resourceId']['videoId']);
+                    $video->setUrl('https://www.youtube.com/embed/' . $videoData['snippet']['resourceId']['videoId']);
                     $video->setCreatedAt(new DateTimeImmutable($videoData['snippet']['publishedAt']));
                     // Je définis la catégorie pour les videos 
                     $category = $entityManager->getRepository(Category::class)->find(2);
@@ -61,60 +61,14 @@ class ActionsController extends AbstractController
                     $videoEntities[] = $video;
                 
                 }
-            } catch (\Exception $e) {
-                // Afficher les éventuelles erreurs de persistance
-                dd($e->getMessage('Ca marche pas mec'));
-            }
-
-            $entityManager->flush();
-
-            // filtrer les vidéos en fonction de l'année de publication
-            if ($order == '2023') {
-                $videoEntities = array_filter($videoEntities, function ($video) use ($order) {
-                    $publishedAt = $video->getCreatedAt();
-                    return $publishedAt->format('Y') == $order;
-                });
-            }
-            // filtrer les vidéos en fonction de l'année de publication
-            if ($order == '2022') {
-                $videoEntities = array_filter($videoEntities, function ($video) use ($order) {
-                    $publishedAt = $video->getCreatedAt();
-                    return                     $publishedAt->format('Y') == $order;
-                });
-            }
-
-            if ($order == 'Cheffe') {
-                // Trier les vidéos par date de publication (de la plus récente à la plus ancienne)
-                usort($videoEntities, function ($a, $b) {
-                    return $b->getCreatedAt() <=> $a->getCreatedAt();
-                });
-            }
-
-            if ($order == 'desc') {
-                // Trier les vidéos par date de publication (de la plus récente à la plus ancienne)
-                usort($videoEntities, function ($a, $b) {
-                    return $b->getCreatedAt() <=> $a->getCreatedAt();
-                });
-            } elseif ($order == 'asc') {
-                // Trier les vidéos par date de publication (de la plus ancienne à la plus récente)
-                usort($videoEntities, function ($a, $b) {
-                    return $a->getCreatedAt() <=> $b->getCreatedAt();
-                });
-            }
-
-            if ($category) {
-                $videoEntities = $entityManager->getRepository(Video::class)->findBy(['category' => $category]);
-                return $videoEntities;
-            } else {
-                $videoEntities = $entityManager->getRepository(Video::class)->findAll();
-            }
-
-            return $videoEntities;
-        });
-
-        return $this->render('actions/index.html.twig', [
-            'controller_name' => 'ActionsController',
-            'videos' => $videoEntities,
-        ]);
+                $entityManager->flush(); 
+        
+            
+            return $this->render('actions/index.html.twig', [
+                'controller_name' => 'ActionsController',
+                'videos' => $videoEntities
+            ]);
+            
     }
+
 }
