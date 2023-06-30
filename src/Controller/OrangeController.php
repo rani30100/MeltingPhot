700f9bb4df30bd134fe10a602fb4a1cac154e9ca
Rani Derradj <raniderradj2@gmail.com>
2023-06-30 12:51:05 +0200

    ajout section page actions

---
+++ src/Controller/OrangeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class OrangeController extends AbstractController
+{
+    #[Route('/orange', name: 'app_orange')]
+    public function index(): Response
+    {
+      
+
+        $jsFiles = [
+            'http://127.0.0.1:8000/js/flipbook/lib/turn.js',
+            'http://127.0.0.1:8000/js/flipbook/lib/zoom.min.js',
+            'http://127.0.0.1:8000/js/flipbook/magazine.js',
+        ];
+        $cssFiles = [
+            'assets/styles/flipbook/magazine.css',
+           
+        ];
+       
+       
+
+        return $this->render('orange/index.html.twig', [
+            'controller_name' => 'OrangeController',
+            'jsFiles' => $jsFiles,
+            'cssFiles' => $cssFiles,
+        ]);
+    }
+}
