e773e1d817b0214ac3aaf79a0753aac16ea52505
Rani Derradj <raniderradj2@gmail.com>
2023-07-03 13:59:51 +0200

    test zoom

--- public/js/flipbook/main.js
+++ public/js/flipbook/main.js
@@ -80,7 +80,15 @@
   
         currentLocation--;
       }
-
+      
+      function zoom(scale) {
+        document.body.style.transform = "scale(" + scale + ")";
+        document.body.style.transformOrigin = "top left";
+        document.body.style.width = (100 / scale) + "%";
+        document.body.style.height = (100 / scale) + "%";
+    };
+    zoom(1.25);
+      
     }
 
  
