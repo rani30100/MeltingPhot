0d2279efb6d15f1a9f18f3708d4a90003e88ec0e
Rani Derradj <raniderradj2@gmail.com>
2023-07-06 16:23:11 +0200

    pages erreur et google analytics fait a attendre 48H

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -87,7 +87,7 @@ class DashboardController extends AbstractDashboardController
         // ->setAction(Crud::PAGE_NEW);
 
         yield MenuItem::section('Statistiques');
-        yield MenuItem::linktoRoute('Mes Stats', 'fa fa-chart-bar', Business::class);
+        yield MenuItem::linkToUrl('Mes Stats', 'fa fa-chart-bar', '/business');
         //     // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
         //     yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
 
--- src/Controller/BusinessController.php
+++ src/Controller/BusinessController.php
@@ -1,87 +1,47 @@
 <?php
-
 namespace App\Controller;
 
-use Google\Client as GoogleClient;
-use Google\Service\AnalyticsReporting;
+use Google\Client;
+use Google\Service\Analytics;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 
 class BusinessController extends AbstractController
 {
     #[Route('/business', name: 'app_business')]
     public function index(): Response
     {
-             // Configure Google API client
-             $client = new GoogleClient();
-             $client->setAuthConfig('Google/meltingphot-Business.json');
-             // Remplacez par le chemin vers votre fichier JSON de configuration client
-             $client->addScope('https://www.googleapis.com/auth/analytics.readonly');
-     
-             // Authentification et création du service Analytics Reporting
-             $client->fetchAccessTokenWithAssertion();
-             $accessToken = $client->getAccessToken();
-             $analytics = new \Google_Service_AnalyticsReporting($client);
-     
-             // ID de la vue (profil) Google Analytics
-             $viewId = '5419813494';
-     
-             // Date de début et de fin pour les statistiques
-             $startDate = new \DateTime('2023-01-01');
-             $endDate = new \DateTime();
-     
-             // Création de la requête de rapport
-             $request = new \Google_Service_AnalyticsReporting_ReportRequest();
-             $request->setViewId($viewId);
-             $request->setDateRanges([
-                 new \Google_Service_AnalyticsReporting_DateRange([
-                     'startDate' => $startDate->format('Y-m-d'),
-                     'endDate' => $endDate->format('Y-m-d'),
-                 ]),
-             ]);
-             $request->setMetrics([
-                 new \Google_Service_AnalyticsReporting_Metric([
-                     'expression' => 'ga:users',
-                 ]),
-                 new \Google_Service_AnalyticsReporting_Metric([
-                     'expression' => 'ga:sessions',
-                 ]),
-                 new \Google_Service_AnalyticsReporting_Metric([
-                     'expression' => 'ga:pageviews',
-                 ]),
-             ]);
-     
-             // Création de la requête de rapport
-             $reportRequest = new \Google_Service_AnalyticsReporting_GetReportsRequest();
-             $reportRequest->setReportRequests([$request]);
-             $reports = $analytics->reports->batchGet($reportRequest);
-     
-             // Traitement de la réponse du rapport
-             $report = $reports[0];
-             $rows = $report->getData()->getRows();
-     
-             $stats = [];
-             if (!empty($rows)) {
-                 $usersMetric = $rows[0]->getMetrics()[0];
-                 $sessionsMetric = $rows[0]->getMetrics()[1];
-                 $pageviewsMetric = $rows[0]->getMetrics()[2];
-     
-                 $stats = [
-                     'users' => $usersMetric->getValues()[0],
-                     'sessions' => $sessionsMetric->getValues()[0],
-                     'pageviews' => $pageviewsMetric->getValues()[0],
-                 ];
-             }
-     
-             // Afficher les statistiques sur votre template
-             return $this->render('stats/site_stats.html.twig', [
-                 'stats' => $stats,
-             ]);
+        // Instantiate the Google\Client
+        $client = new Client();
+        // Set the path to the JSON file containing the credentials
+        $client->setAuthConfig('/var/www/html/Projet-MeltingPhot/config/google_credentials.json');
+        
+        // Authorize the client
+        $client->setScopes([
+            'https://www.googleapis.com/auth/analytics.readonly',
+        ]);
+        
+        // Create a new Google_Service_Analytics object
+        $analyticsService = new \Google_Service_Analytics($client);
+        
+        // Make a request to retrieve the analytics data
+        $analyticsData = $analyticsService->data_ga->get(
+            'ga:5770624721', // Replace with your Google Analytics View ID
+            '2023-07-01',   // Start date
+            '2023-07-31',   // End date
+            'ga:users,ga:sessions,ga:pageviews' // Metrics to retrieve
+        );
+        
+        // Extract the statistics from the response
+        $stats = [
+            'users' => $analyticsData['totalsForAllResults']['ga:users'],
+            'sessions' => $analyticsData['totalsForAllResults']['ga:sessions'],
+            'pageviews' => $analyticsData['totalsForAllResults']['ga:pageviews'],
+        ];
          
         return $this->render('business/index.html.twig', [
-            'controller_name' => 'BusinessController',
+            'stats' => $stats,
         ]);
     }
 }
