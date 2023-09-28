02ae777e7e75195f3eb14ea2f2d2bf0f29cfb1b8
Rani Derradj <raniderradj2@gmail.com>
2023-09-28 14:09:43 +0200

    ajout user ImageCrud

--- src/Controller/Admin/ImageCrudController.php
+++ src/Controller/Admin/ImageCrudController.php
@@ -4,8 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Page;
 use App\Entity\Image;
+use Twig\Environment;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Form\FormBuilderInterface;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -21,6 +23,17 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ImageCrudController extends AbstractCrudController
 {
+
+    private Environment $twig;
+    private Security $security; // Inject the Security service
+
+    // Inject the Twig Environment and Security service using constructor injection
+    public function __construct(Environment $twig, Security $security)
+    {
+        $this->twig = $twig;
+        $this->security = $security;
+    }
+    
     public static function getEntityFqcn(): string
     {
         return Image::class;
@@ -29,6 +42,11 @@ class ImageCrudController extends AbstractCrudController
     
     public function configureFields(string $pageName): iterable
     { 
+        yield AssociationField::new('user', 'Utilisateur')
+        ->setLabel('Utilisateur')
+        ->setCustomOption('user', $this->security->getUser()) // Passer l'utilisateur actuel au champ
+        ->hideOnForm(); // Cacher le champ dans le formulaire
+
         yield TextField::new('title', "Titre ");
 
         yield ImageField::new('path', 'Image')
@@ -62,34 +80,58 @@ class ImageCrudController extends AbstractCrudController
    
     public function createEditFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
     {
-
         $builder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
-
-        $builder
-            ->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
-              
-                $this->addFlash(
-                    'success', // Le type du message flash (par exemple, 'success' pour un message de succès)
-                    'Les modifications ont été enregistrées avec succès!' // Le message à afficher
-                );
-            });
-
+    
+        $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+            // Récupérez les données soumises dans le formulaire
+            $formData = $event->getData();
+            
+            // Vérifiez si les données sont une instance de la classe "Image" (votre entité) et si le champ "user" est vide
+            if ($formData instanceof Image && !$formData->getUser()) {
+                // Récupérez l'utilisateur actuellement authentifié à partir du service "Security"
+                $user = $this->security->getUser();
+            
+                // Remplissez le champ "user" de l'entité "Image" avec l'utilisateur actuel
+                $formData->setUser($user);
+            }
+            
+            // Ajoutez un message flash de succès pour informer l'utilisateur que les modifications ont été enregistrées avec succès
+            $this->addFlash(
+                'success',
+                'Les modifications ont été enregistrées avec succès!'
+            );
+        });
+        
+    
         return $builder;
     }
+
     public function createNewFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
     {
 
         $builder = parent::createNewFormBuilder($entityDto, $formOptions, $context);
 
-        $builder
-            ->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
-  
-                $this->addFlash(
-                    'success', // Le type du message flash (par exemple, 'success' pour un message de succès)
-                    'Les modifications ont été enregistrées avec succès!' // Le message à afficher
-                );
-            });
+        $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+            // Récupérez les données soumises dans le formulaire
+            $formData = $event->getData();
+            
+            // Vérifiez si les données sont une instance de la classe "Image" (votre entité) et si le champ "user" est vide
+            if ($formData instanceof Image && !$formData->getUser()) {
+                // Récupérez l'utilisateur actuellement authentifié à partir du service "Security"
+                $user = $this->security->getUser();
+            
+                // Remplissez le champ "user" de l'entité "Image" avec l'utilisateur actuel
+                $formData->setUser($user);
+            }
+            
+            // Ajoutez un message flash de succès pour informer l'utilisateur que les modifications ont été enregistrées avec succès
+            $this->addFlash(
+                'success',
+                'Les modifications ont été enregistrées avec succès!'
+            );
+        });
 
         return $builder;
     }
+    
 }
