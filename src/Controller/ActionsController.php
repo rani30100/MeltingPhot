2254d2e4c0774f61c329e44445a6d1e49c7e8738
Rani Derradj <raniderradj2@gmail.com>
2023-09-29 03:33:41 +0200

    Reparation cache videos

--- src/Controller/ActionsController.php
+++ src/Controller/ActionsController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use Google\Client;
@@ -13,6 +14,7 @@ use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ActionsController extends AbstractController
@@ -51,104 +53,95 @@ class ActionsController extends AbstractController
 
 
     #[Route('/actions/{id}', name: 'app_actions_video')]
-    public function redirectToVideo( int $id, VideoRepository $videoRepository): Response
+    public function redirectToVideo(int $id, VideoRepository $videoRepository): Response
     {
-      // Récupérer la vidéo à partir de l'identifiant (ID)
-      $video = $videoRepository->find($id);
- 
-      $videos = $videoRepository->findAll();
-      // Si la vidéo n'est pas trouvée, vous pouvez gérer l'erreur ici ou rediriger l'utilisateur vers une page d'erreur.
-      if (!$video) {
-          throw $this->createNotFoundException('La vidéo n\'existe pas.');
-      }
+        // Récupérer la vidéo à partir de l'identifiant (ID)
+        $video = $videoRepository->find($id);
 
-      // Rendre le template de la vue qui affiche le modal avec la vidéo correspondante
-      return $this->render('actions/modal_video.html.twig', [
-          'video' => $video,
-          'videos' => $videos
-      ]);
-  }
-    
+        $videos = $videoRepository->findAll();
+        // Si la vidéo n'est pas trouvée, vous pouvez gérer l'erreur ici ou rediriger l'utilisateur vers une page d'erreur.
+        if (!$video) {
+            throw $this->createNotFoundException('La vidéo n\'existe pas.');
+        }
+
+        // Rendre le template de la vue qui affiche le modal avec la vidéo correspondante
+        return $this->render('actions/modal_video.html.twig', [
+            'video' => $video,
+            'videos' => $videos
+        ]);
+    }
+
 
 
     #[Route('/actions/{category}', defaults: ['category' => 'Je_Filme_Mon_Futur_Métier'], methods: ['GET', 'HEAD'], name: 'app_actions')]
-    public function index(string $category = null, EntityManagerInterface $entityManager,EbookRepository $ebooks, CacheInterface $cache, VideoRepository $videoRepository): Response
+    public function index(string $category = null, EntityManagerInterface $entityManager, EbookRepository $ebooks, CacheInterface $cache, VideoRepository $videoRepository): Response
     {
-        // Récupération du Ebook
-        $ebooks = $ebooks->findAll();
-        $videos = $videoRepository->findAll();
-        // Essaye de récupérer les vidéos depuis le cache s'ils sont disponibles
-        if (!empty($videos)) {
 
-            $cachedVideos = $cache->get('playlist_videos', function (ItemInterface $item) use ($category, $entityManager) {
-                
-                // Récupération des vidéos depuis la base de données pour la catégorie spécifiée
-                $videoRepository = $entityManager->getRepository(Video::class);
-                $videos = $videoRepository->findByCategory($category);
+        $cache = new FilesystemAdapter();
+        // Essayez de récupérer les vidéos depuis le cache s'ils sont disponibles
+        $cachedVideos = $cache->getItem('playlist_videos');
 
-                // Stocker les vidéos récupérées dans le cache pour éviter de rétélcharger les vidéos
-                $item->expiresAfter(3600); // Cache pendant 1 heure
-                $item->set($videos);
-                return $videos;
-            });
-        }else{
-            // dd($videos);
-             // Si aucune vidéo n'existe pour la catégorie dans la base de données, récupérez-les depuis YouTube et stockez-les
-            //Récupéraiton de la playlist
-            $youtubeVideos = $this->fetchYouTubeVideos();
-            // dd($youtubeVideos);
-            // Si il trouve la playlist
-            if ($youtubeVideos) {
-                $imageIndex = 0;
-                //Associe chaque vidéo avec l'id de la vidéo 
-                foreach ($youtubeVideos as $youtubeVideo) {
-                    $videoUrl = 'https://www.youtube.com/embed/' . $youtubeVideo->getSnippet()->getResourceId()->getVideoId();
+        if (!$cachedVideos->isHit()) {
+            // Le cache est vide, récupérez les vidéos depuis la base de données ou YouTube
+            $videos = $videoRepository->findAll();
 
-                    // Vérifier si la vidéo avec la même URL existe déjà dans la base de données
-                    $existingVideo = $videoRepository->findOneBy(['url' => $videoUrl]);
-                    
-                    if (!$existingVideo) {
-                        // La vidéo n'existe pas dans la base de données, ça la créee
-                        $video = new Video();
-                        $video->setTitle($youtubeVideo->getSnippet()->getTitle());
-                        $video->setUrl($videoUrl);
-                        $video->setCreatedAt(new DateTimeImmutable($youtubeVideo->getSnippet()->getPublishedAt()));
-                        $category = $entityManager->getRepository(Category::class)->find(2);
-                        $video->setCategory($category);
+            if (empty($videos)) {
+                // Si les vidéos ne sont pas en base de données, récupérez-les depuis YouTube
+                $youtubeVideos = $this->fetchYouTubeVideos();
 
-                        // Obtenez l'URL de la miniature en utilisant l'ID de la vidéo
-                        // $thumbnailUrl = $this->getImage($youtubeVideo->getSnippet()->getResourceId()->getVideoId());
-                        $video->setImage(null);
+                if ($youtubeVideos) {
+                    $videos = [];
 
-                        $entityManager->persist($video);
+                    foreach ($youtubeVideos as $youtubeVideo) {
+                        $videoUrl = 'https://www.youtube.com/embed/' . $youtubeVideo->getSnippet()->getResourceId()->getVideoId();
 
-                        // Incrémentez l'index de l'image pour la vidéo suivante
-                        $imageIndex++;
+                        // Vérifiez si la vidéo avec la même URL existe déjà dans la base de données
+                        $existingVideo = $videoRepository->findOneBy(['url' => $videoUrl]);
+
+                        if (!$existingVideo) {
+                            // La vidéo n'existe pas dans la base de données, créez-la
+                            $video = new Video();
+                            $video->setTitle($youtubeVideo->getSnippet()->getTitle());
+                            $video->setUrl($videoUrl);
+                            $video->setCreatedAt(new DateTimeImmutable($youtubeVideo->getSnippet()->getPublishedAt()));
+                            $categoryEntity = $entityManager->getRepository(Category::class)->find(2);
+                            $video->setCategory($categoryEntity);
+                            $video->setImage(null);
+
+                            $entityManager->persist($video);
+
+                            // Ajoutez la vidéo à la liste des vidéos
+                            $videos[] = $video;
+                        }
                     }
+
+                    $entityManager->flush();
                 }
 
-                $entityManager->flush();
-                $videos = $videoRepository->findByCategory($category);
+                // Mettre en cache les vidéos
+                $cachedVideos->set($videos);
+                $cache->save($cachedVideos);
+            } else {
+                // Mettre en cache les vidéos récupérées depuis la base de données
+                $cachedVideos->set($videos);
+                $cache->save($cachedVideos);
             }
-            
+        } else {
+            // Si les vidéos ne sont pas récupérer depuis la BDD ou YTB alors récupere les vidéos depuis le cache
+            $videos = $cachedVideos->get();
         }
 
-        // Si les vidéos récupérées sont vides et que la catégorie n'est pas celle par défaut, rend le template "novideo.html.twig"
-        if ($category !== 'Je_Filme_Mon_Futur_Métier' && empty($cachedVideos)) {
+        // Vérifiez si les vidéos récupérées sont vides et que la catégorie n'est pas celle par défaut
+        if ($category !== 'Je_Filme_Mon_Futur_Métier' && empty($videos)) {
             return $this->render('actions/no_videos.html.twig', [
-            'category' => $category
-        ]);
+                'category' => $category
+            ]);
         }
-
-
-        // S'il il n'y a pas de videos misent en cache, prend les videos de la BDD
-        $videosToShow = $cachedVideos ?: $videos;
 
         return $this->render('actions/index.html.twig', [
-            'videos' => $videosToShow,
+            'videos' => $videos,
             'ebooks' => $ebooks,
             'category' => $category
-
         ]);
     }
 }
